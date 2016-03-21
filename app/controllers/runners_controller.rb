cc82ff1885f158284f5356148a4d82eab1bf74fc
Hilary Masland <hilarymasland@gmail.com>
2016-03-21 15:14:54 -0400

    Continuing to connect files / minimal CSS

--- app/controllers/runners_controller.rb
+++ app/controllers/runners_controller.rb
@@ -1,6 +1,11 @@
 class RunnersController < ApplicationController
+  def index
+    @runners = Runner.all
+  end
   def new
+    @runner = Runner.new
   end
   def create
+    @runner = Runner.new
   end
 end
