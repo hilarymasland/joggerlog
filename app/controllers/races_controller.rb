530b3dc3c1a4f03a1ac66a23087b70bba7719c62
Hilary Masland <hilarymasland@gmail.com>
2016-03-22 09:52:07 -0400

    CRUD!

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -5,11 +5,11 @@ class RacesController < ApplicationController
   end
   def show
     @runner = Runner.find(params[:runner_id])
-    @race = @runner.races.create(race_params)
+    @race = @runner.races.find(params[:id])
   end
   def new
     @runner = Runner.find(params[:runner_id])
-    @race = Race.new
+    @race = @runner.races.new
   end
   def create
     @runner = Runner.find(params[:runner_id])
@@ -17,6 +17,24 @@ class RacesController < ApplicationController
 
     redirect_to runner_races_path(@runner, @races)
   end
+  def edit
+    @runner = Runner.find(params[:runner_id])
+    @race = @runner.races.find(params[:id])
+  end
+  def update
+    @runner = Runner.find(params[:runner_id])
+    @race = @runner.races.find(params[:id])
+    @race.update(race_params)
+
+    redirect_to runner_races_path
+  end
+  def destroy
+  @runner = Runner.find(params[:runner_id])
+  @race = Race.find(params[:id])
+  @race.destroy
+
+  redirect_to runner_races_path
+end
   private
   def race_params
     params.require(:race).permit(:title, :date, :time)
